e1654b81ec60268c70b35e0012fc8fb8c6d050fc
Marcel B <marcel@beestig.nl>
2018-04-11 00:20:25 +0200

    cookbook defaults to 9.6 so test with 9.6

--- test/cookbooks/test/recipes/server_install.rb
+++ test/cookbooks/test/recipes/server_install.rb
@@ -1,14 +1,14 @@
 include_recipe 'locale::default'
 
 postgresql_repository 'install' do
-  version '9.5'
+  version '9.6'
 end
 
 postgresql_server_install 'package' do
-  version '9.5'
+  version '9.6'
 end
 
 postgresql_server_conf 'PostgreSQL Config' do
-  version '9.5'
+  version '9.6'
   notification :reload
 end
